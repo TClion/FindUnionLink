39fad23c8636a90adffbb64e0645351bb07c76e1
TClion <763242665@qq.com>
2018-12-18 21:51:05 +0800

    20181218

--- findcoin.py
+++ findcoin.py
@@ -7,22 +7,35 @@
 # create:2018-12-12
 
 import re
+import sys
 import requests
 
 
-def find(url=None, html=None):
+def get_html(url):
     try:
         html = requests.get(url).text
+        return html
     except:
         print "Page cannot be accessed"
-    coinpage = re.compile(r'https://coinhive.com/lib/coinhive.min.js')
-    result = coinpage.search(html)
+        sys.exit(0)
+
+def find_coinhive_js(html):
+    coinjs = re.compile(r'https://coinhive.com/lib/coinhive.min.js')
+    result = coinjs.search(html)
     if result:
         print 'This page have coinhive code'
-    else:
-        print 'safe html'
+
+
+def find_jd_union_link(html):
+    jdurl = re.compile(r'https://union-click.jd.com/jdc\?d=')
+    result = jdurl.search(html)
+    if result:
+        print 'This page have jd union link'
+
 
 
 if __name__ == '__main__':
     url = "http://www.baidu.com"
-    find(url)
+    html = get_html(url)
+    find_coinhive_js(html)
+    find_jd_union_link(html)
